f6044f9dd84c8e80099d427633570a3d2d070a7e
Virginia Piassentini <virginiapiassentini@gmail.com>
2020-02-15 10:46:08 -0300

    Adding css reset :bathtub:

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: ['node_modules/css-reset-and-normalize-sass/scss']
+    }
   });
 
   return app.toTree();
